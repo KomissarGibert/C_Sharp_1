ec293deb83d7a41ba60919a345ca6589815cef37
kanniboul <korben.dallas.88@yandex.ru>
2022-08-04 21:53:32 +0300

    SumSum

--- HelloSumm_3/Program.cs
+++ HelloSumm_3/Program.cs
@@ -1,4 +1,4 @@
-﻿int numberA = new Random().Next(123, 567887); // случайное числа в диапозоне
+﻿int numberA = new Random().Next(123, 567887-1); // случайное числа в диапозоне
 Console.WriteLine(numberA); //Выводим это случайное число
 int numberB = new Random().Next(23, 76); // случайное числа в диапозоне
 Console.WriteLine(numberB); //Выводим это случайное число
