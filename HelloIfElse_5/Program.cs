ef23e5be754fa3e840de278b4bf862c81bc42ded
kanniboul <korben.dallas.88@yandex.ru>
2022-08-04 22:13:39 +0300

    if_else

--- HelloIfElse_5/Program.cs
+++ HelloIfElse_5/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter name user");
+string username = Console.ReadLine();
+
+if(username.ToUpper() == "МАША")
+{
+    Console.WriteLine("Машка ты чтоли, ни чё се!!!");
+}
+else
+{
+    Console.Write("Ну здравствуй НЕ МАША, а ");
+    Console.WriteLine(username);
+}
