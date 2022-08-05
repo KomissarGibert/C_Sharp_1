7d550595961aa6abc657a729d733ffd656b1b52e
kanniboul <korben.dallas.88@yandex.ru>
2022-08-05 22:22:53 +0300

    Сравнение числе

---
+++ Task002_сравнение/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int m = Convert.ToInt32(Console.ReadLine());
+
+if(n == m*m)
+{
+    Console.WriteLine("да");
+}
+
+else
+{
+    Console.WriteLine("нет");
+}
